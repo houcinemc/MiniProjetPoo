7e4a7417b606a6957d688701448576963925a255
Fatima-YF <f.ucf.eva@gmail.com>
2017-01-04 18:46:07 +0100

    fixed the bug

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,16 +3,12 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.CheckBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 import javafx.scene.image.ImageView;
 
